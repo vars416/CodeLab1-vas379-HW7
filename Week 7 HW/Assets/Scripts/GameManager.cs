eb869f88894020920424add8ac6740b162398700
vars416 <56270900+vars416@users.noreply.github.com>
2020-04-06 02:03:59 -0400

    50% HW done

---
+++ Week 7 HW/Assets/Scripts/GameManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text title; //holds ui title
+    public Text description;  //holds ui description
+
+    public Button Forward;  //north 
+    public Button Backward;  //south
+    public Button Right;  //east
+    public Button Left;  //west
+
+    public int numLocations;
+
+    public Location currentLocation; //the current location
+
+    public Location[] locations; //array of all the locations
+
+    string filePath = "/TextFiles/Location<num>.json"; //default path to location files
+
+    //once at the start
+    void Start()
+    {
+        filePath = Application.dataPath + filePath; //full path to files
+
+        locations = new Location[numLocations]; //init array to have numLocation slots
+
+        for (int i = 0; i < locations.Length; i++)
+        { //0 to locations.Length
+            string locPath = filePath.Replace("<num>", "" + i); //creating a path to file num "i"
+
+            string fileContent = File.ReadAllText(locPath); //fileContent will hold all the text from the file at locPath
+
+            Location l = JsonUtility.FromJson<Location>(fileContent);
+
+            locations[i] = l;
+        }
+
+        UpdateLocation(0);
+    }
+
+    public void GoFront()
+    {
+        UpdateLocation(currentLocation.ForwardLocation);
+    }
+
+    public void GoBack()
+    {
+        UpdateLocation(currentLocation.BackwardLocation);
+    }
+
+    public void GoRight()
+    {
+        UpdateLocation(currentLocation.RightLocation);
+    }
+
+    public void GoLeft()
+    {
+        UpdateLocation(currentLocation.LeftLocation);
+    }
+
+    public void UpdateLocation(int locNum)
+    {
+        currentLocation = locations[locNum];
+
+        title.text = currentLocation.title;
+        description.text = currentLocation.description;
+
+        if (currentLocation.ForwardLocation < 0)
+        {
+            Forward.gameObject.SetActive(false);
+        }
+        else
+        {
+            Forward.gameObject.SetActive(true);
+        }
+
+        if (currentLocation.BackwardLocation < 0)
+        {
+            Backward.gameObject.SetActive(false);
+        }
+        else
+        {
+            Backward.gameObject.SetActive(true);
+        }
+
+        if (currentLocation.RightLocation < 0)
+        {
+            Right.gameObject.SetActive(false);
+        }
+        else
+        {
+            Right.gameObject.SetActive(true);
+        }
+
+        if (currentLocation.LeftLocation < 0)
+        {
+            Left.gameObject.SetActive(false);
+        }
+        else
+        {
+            Left.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Week 7 HW/Assets/Scripts/Location.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Location // a Location Class (not a component)
+{
+    public string title;        //var for a title
+    public string description;  //var for a description
+
+    public int ForwardLocation = -1; //var for a location num for N
+    public int BackwardLocation = -1; //var for a location num for S
+    public int RightLocation = -1; //var for a location num for E
+    public int LeftLocation = -1; //var for a location num for W
+
+
+    //Constructor that takes only a title and a description
+    public Location(string title, string description)
+    {
+        this.title = title; //"this.title" means "this objects title". just "title" means the parameter passed to this function
+        this.description = description;  //"this.description" means "this objects description". just "description" means the parameter passed to this function
+    }
+
+    //Constructor that takes only a title, description, and ints for n, s, e, w 
+    public Location(string title, string description,
+                    int F, int B, int R, int L)
+    {
+        this.title = title;  //"this.title" means "this objects title". just "title" means the parameter passed to this function
+        this.description = description; //"this.description" means "this objects description". just "description" means the parameter passed to this function
+
+        ForwardLocation = F;
+        BackwardLocation = B;
+        RightLocation = R;
+        LeftLocation = L;
+    }
+}
